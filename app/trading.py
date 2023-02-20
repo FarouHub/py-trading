93c7ca3cdbd28483d8f5b8d13e6006461dfc0d65
FarouHub <sittler.n@gmail.com>
2023-02-20 16:34:35 +0100

    make it work with binance

--- app/trading.py
+++ app/trading.py
@@ -6,14 +6,24 @@ from plotly.subplots import make_subplots
 from datetime import datetime
 
 df = pd.read_csv('../datas/BTCUSDT-5m-2023-02-18.csv')
+
+# df['open_time'] = df['open_time'].dt.date
+df['open_time'] = pd.to_datetime(df['open_time'], unit='ms')
+# df['open_time'] = datetime.fromtimestamp(math.floor(df['open_time'].str))
+# df['open_time'] = pd.to_datetime(df['open_time'], format = '%d.%m.%Y %H:%M:%S')
+
+# print(datetime.fromtimestamp(math.floor(1676678699)))
+df.set_index('open_time', inplace=True)
+
 df = df[df.high != df.low]
+
 
 df['VWAP'] = ta.vwap(df.high, df.low, df.close, df.volume)
 df['RSI'] = ta.rsi(df.close, length = 16)
 my_bbands = ta.bbands(df.close, length = 14, std = 2.0)
 df = df.join(my_bbands)
 
-
+####
 VWAPsignal = [0] * len(df)
 backcandles = 15
 
@@ -39,10 +49,10 @@ df['VWAPsignal'] = VWAPsignal
 
 ########
 def TotalSignal(l):
-    if(df.VWAPsignal[l] == 2 and df.close[l] <= df['BBL_14_2.0'] and df.RSI[l] < 45):
+    if(df.VWAPsignal[l] == 2 and df.close[l] <= df['BBL_14_2.0'][l] and df.RSI[l] < 45):
         return 2
     
-    if(df.VWAPsignal[l] == 1 and df.close[l] >= df['BBU_14_2.0'] and df.RSI[l] > 55):
+    if(df.VWAPsignal[l] == 1 and df.close[l] >= df['BBU_14_2.0'][l] and df.RSI[l] > 55):
         return 1
     
     return 0
@@ -56,17 +66,20 @@ df['TotalSignal'] = TotSignal
 
 df[df.TotalSignal != 0].count()
 
+
 ########
 def pointposbreak(x):
-    if x['TotalSIgnal'] == 1:
+    if x['TotalSignal'] == 1:
         return x['high'] + 1e-4
-    elif x['TotalSIgnal'] == 2:
+    elif x['TotalSignal'] == 2:
         return x['low'] - 1e-4
     else:
         return np.nan
     
 df['pointposbreak'] = df.apply(lambda row: pointposbreak(row), axis = 1)
 
+df.to_csv('../datas/file.csv')
+print(df)
 ########
 st = 10400
 dfpl = df[st:st+350]
